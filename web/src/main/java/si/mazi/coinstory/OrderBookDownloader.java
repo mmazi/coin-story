901575e9e4272baeace8107f5939fb6a7c78dea3
matija.mazi@gmail.com <matija.mazi@gmail.com>
2013-03-30 19:16:15 +0100

    Round time down to the minute; minor improvements

--- web/src/main/java/si/mazi/coinstory/OrderBookDownloader.java
+++ web/src/main/java/si/mazi/coinstory/OrderBookDownloader.java
@@ -4,6 +4,7 @@ import com.google.common.collect.Iterables;
 import com.xeiam.xchange.dto.marketdata.OrderBook;
 import com.xeiam.xchange.dto.trade.LimitOrder;
 import com.xeiam.xchange.service.marketdata.polling.PollingMarketDataService;
+import org.joda.time.DateTime;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -27,16 +28,16 @@ public class OrderBookDownloader {
 
     @TransactionAttribute(TransactionAttributeType.REQUIRES_NEW)
     public void readData(PollingMarketDataService exchange, String currency, String service) throws IOException {
-        Date now = new Date();
+        Date now = new DateTime().withSecondOfMinute(0).withMillis(0).toDate();
         OrderBook orderBook;
-        log.info("Connecting to {} for {}...", exchange, currency);
+        log.info("Connecting to {} for {}...", service, currency);
         try {
             orderBook = exchange.getFullOrderBook("BTC", currency);
         } catch (Exception e) {
-            throw new IOException("Error connection to exchange", e);
+            throw new IOException("Error connecting to exchange", e);
         }
         int i = 0;
-        log.info("Got {} bids and {} asks.", orderBook.getBids().size() + orderBook.getAsks().size());
+        log.info("Got {} bids and {} asks.", orderBook.getBids().size(), orderBook.getAsks().size());
         for (LimitOrder limitOrder : Iterables.concat(orderBook.getAsks(), orderBook.getBids())) {
             em.persist(new Ord(limitOrder.getType(), limitOrder.getTradableAmount().doubleValue(), limitOrder.getLimitPrice().getAmount().doubleValue(), now, service, currency));
             if (++i % 100 == 0) {
