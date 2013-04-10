d49cd7fe31df739995c8beb756973f9367bf320a
matija.mazi@gmail.com <matija.mazi@gmail.com>
2013-04-10 17:01:17 +0200

    sql

--- web/src/test/sql/coin-story.sql
+++ web/src/test/sql/coin-story.sql
@@ -2,26 +2,28 @@ select service, count(*)
 from ord group by service
 
 
--- Ask below X USD/BTC
-select o."time", sum(o.amount), count(*), max(o.price) as up_to_price, max(t.last) as current_price, max(o.ordertype) as type, max(o.service) as service
-from ord o
-left join tick t on t.timestamp = o.time and t.currency = o.currency and t.service = o.service
-where o.currency = 'USD'
- and o.service = 'MtGoxExchange'
- and o.ordertype = 'ASK'
- and o.price <= 16000
-group by o."time"
-order by o."time" desc
-
 -- Bids above X USD/BTC in USD
-select o."time", sum(o.amount * o.price), count(*), min(o.price) as above_price, max(t.last) as current_price, max(o.ordertype) as type, max(o.service) as service
+select o."time", sum(o.amount * o.price *
+  (case when o.currency = 'EUR' then 1.3 when o.currency = 'USD' then 1 else 0 end)
+), count(*), min(o.price) as above_price, 
+	(select avg(t.last) from tick t where t.timestamp = o.time and t.currency = 'USD' and t.service = 'MtGoxExchange') as current_price,
+max(o.ordertype) as type, count(distinct o.service) as services
 from ord o
-left join tick t on t.timestamp = o.time and t.currency = o.currency and t.service = o.service
-where o.currency = 'USD'
- and o.service = 'MtGoxExchange'
- and o.ordertype = 'BID'
+where o.ordertype = 'BID'
  and o.price >= 00
 group by o."time"
+having count(distinct o.service) >= 4
 order by o."time" desc
 
+
+-- Ask below X USD/BTC
+select o."time", sum(o.amount), count(*), max(o.price) as up_to_price, 
+	(select avg(t.last) from tick t where t.timestamp = o.time and t.currency = 'USD' and t.service = 'MtGoxExchange') as current_price,
+max(o.ordertype) as type, count(distinct o.service) as services
+from ord o
+where o.ordertype = 'ASK'
+-- and o.price <= 20000
+group by o."time"
+having count(distinct o.service) >= 4
+order by o."time" desc
 
